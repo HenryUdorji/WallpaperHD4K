87b19c9e50eb7dd328cb02435ede7f494d46357a
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-01-09 16:24:18 +0100

    implemented splash screen with the new splash library and custom launcher icon

--- app/src/main/java/com/hashconcepts/wallpaperhd4k/ui/MainActivity.kt
+++ app/src/main/java/com/hashconcepts/wallpaperhd4k/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hashconcepts.wallpaperhd4k.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.hashconcepts.wallpaperhd4k.R
 import com.hashconcepts.wallpaperhd4k.databinding.ActivityMainBinding
 
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_WallpaperHD4K)
+        // Handle the splash screen transition.
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
